b33980f1643007f2f3107091d4da68e93a832ed6
Manh Hung <mrhung189@gmail.com>
2020-06-04 13:37:58 +0700

    Them ghi chu

---
+++ NguyenManhHungLab456/Models/Cource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenManhHungLab456.Models
+{
+    public class Cource
+    {
+        //Them mot dong ghi chu trong model
+    }
+}
